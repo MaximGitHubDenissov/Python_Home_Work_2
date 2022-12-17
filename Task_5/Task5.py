449eaa51d6ae061c4e5f0f7a38ea7aacf847149f
Maxim Denissov <DM7894289@gmail.com>
2022-12-17 15:14:33 +0600

    Add Task 5

---
+++ Task_5/Task5.py
@@ -0,0 +1,7 @@
+# 5 Реализуйте алгоритм перемешивания списка.
+
+import random
+my_list = input('Введите значения через проблел: ').split(' ')
+print (my_list)
+random.shuffle(my_list)
+print (my_list)
